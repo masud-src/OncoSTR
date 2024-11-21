5eadcbbf68fba63354e9f92879a1d3d8224a48a8
marlon <marlon.suditsch@isd.uni-stuttgart.de>
2024-11-21 19:03:21 +0100

    excluded functions into an utils.py file. This will be more user friendly.

--- oncostr/structure_segmentation.py
+++ oncostr/structure_segmentation.py
@@ -1,68 +1,18 @@
 """
-In this white matter segmentation module the heterogeneous distributions are identified.
+In this structure segmentation module the heterogeneous distributions are identified.
 
 Class:
     StructureSegmentation:      Basically uses fast of FSL for the segmentation. Herein, a mixture of Gaussian
                                 distributes the relevant classes of white and grey matter and cerebrospinal fluid in the
                                 brain area.
 """
-import fsl.wrappers.fslmaths
-import fsl.wrappers.fast
 import nibabel as nib
-import numpy as np
-import copy
 import os
 import glob
-from pathlib import Path
-from typing import Union, Any
+import utils
 
 STRUCTURE_SEGMENTATION_PATH = "structure_segmentation" + os.path.sep
-
-def mkdir_if_not_exist(directory: str) -> str:
-    """
-    Makes directory if not exists and returns the string
-
-    *Arguments*:
-        dir: String
-
-    *Example*:
-        dir = mkdir_if_not_exist(dir)
-    """
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    return directory
-
-
-def split_path(s: str) -> tuple[str, str]:
-    """
-    Splits Filepath into file and path
-
-    *Arguments:*
-        s: String
-
-    *Example:*
-        file, path = splitPath(s)
-    """
-    import os
-    f = os.path.basename(s)
-    p = s[:-(len(f)) - 1]
-    return str(f), str(p)
-
-
-def get_path_file_extension(input_file: str) -> tuple[str, str, str]:
-    """
-    Returns path, the filename and the filename without extension.
-
-    *Arguments:*
-        input_file: String
-
-    *Example:*
-        path, file, file_wo_extension = get_path_file_extension(input_file)
-    """
-    file, path = split_path(input_file)
-    file_wo_extension = Path(Path(input_file).stem).stem
-    return path, file, file_wo_extension
-
+MODES = ["tumor_agnostic", "bias_corrected", "tumor_entity_weighted"]
 
 class StructureSegmentation:
     """
@@ -77,7 +27,6 @@ class StructureSegmentation:
         input_files_dir:            List of the input images. It is recommended to only use one image.
         affine:                     Variable for the image affine, every segmentation is mapped
         remove_interim_files:       Bool to remove the created interim files
-        modes:                      All implemented modes for segmentation
         mode:                       Selected mode of segmentation
         brain_handling_classes:     List of brain compartment classes, it should be segmented to
         tumor_class_mapping:        Dict of tumor classes with a respective mapping according to brats segmentation
@@ -109,7 +58,6 @@ class StructureSegmentation:
         self.tumor_seg_dir = None
         self.affine = None
         self.remove_interim_files = True
-        self.modes = ["tumor_agnostic", "bias_corrected", "tumor_entity_weighted"]
         self.mode = "bias_corrected"
         self.brain_handling_classes = ["cerebrospinal_fluid", "gray_matter", "white_matter"]
         self.tumor_class_mapping = {"edema": 2, "active": 4, "necrotic": 1}
@@ -130,71 +78,7 @@ class StructureSegmentation:
 
         :return: None
         """
-        print(self.modes)
-
-    @staticmethod
-    def cut_area_from_image(input_image: str, area_mask: nib.Nifti1Image,
-                            inverse: bool = False) -> Union[None, nib.Nifti1Image]:
-        """
-        Cuts an area of that image.
-
-        :param input_image: String of path to Nifti image
-        :param area_mask: Mask array
-        :param inverse: Bool, true for inverse cut
-
-        :return: optionally returns the image or writes it next to the input image
-        """
-        if inverse:
-            area_mask = fsl.wrappers.fslmaths(area_mask).mul(-1).add(1).run()
-
-        return fsl.wrappers.fslmaths(input_image).mul(area_mask).run()
-
-    @staticmethod
-    def image2array(image_dir: str) -> tuple[Any, Any, Any]:
-        """
-        Takes a directory of an image and gives a numpy array.
-
-        :param image_dir: String of a Nifti image directory
-        :return: numpy array of image data, shape, affine
-        """
-        orig_image = nib.load(image_dir)
-        return copy.deepcopy(orig_image.get_fdata()), orig_image.shape, orig_image.affine
-
-    @staticmethod
-    def image2mask(image_dir: str, compartment: int, inner_compartments: list[int] = None) -> np.ndarray:
-        """
-        Gives deep copy of original image with selected compartments.
-
-        :param image_dir: String to Nifti image
-        :param compartment: Int, identifier of compartment that shall be filtered
-        :param inner_compartments: List of inner compartments that also are included in the mask
-
-        :return mask: Numpy array of the binary mask
-        """
-        mask, _, _ = StructureSegmentation.image2array(image_dir)
-        unique = list(np.unique(mask))
-        unique.remove(compartment)
-        for outer in unique:
-            mask[np.isclose(mask, outer)] = 0.0
-        mask[np.isclose(mask, compartment)] = 1.0
-        if inner_compartments is not None:
-            for comp in inner_compartments:
-                mask[np.isclose(mask, comp)] = 1.0
-                unique.remove(comp)
-        return mask
-
-    @staticmethod
-    def single_segmentation(basename: str, files_list: list[str], n_classes: int) -> None:
-        """
-        runs fast segmentation algorithm in default with variable input files.
-
-        :param basename: String for base name of outputfiles
-        :param files_list: List of input images
-        :param n_classes: Number of segmentation classes
-
-        :return: None
-        """
-        fsl.wrappers.fast(files_list, basename, n_classes)
+        print(MODES)
 
     def set_affine(self, image_dir: str = None) -> None:
         """
@@ -216,17 +100,17 @@ class StructureSegmentation:
         :return:
         """
         seg_id = list(self.tumor_class_mapping.values())
-        tumor_mask = StructureSegmentation.image2mask(self.tumor_seg_dir, seg_id[0], seg_id[1:])
+        tumor_mask = utils.image2mask(self.tumor_seg_dir, seg_id[0], seg_id[1:])
         self.set_affine()
         image_tumor_mask = nib.Nifti1Image(tumor_mask, self.affine)
         for modality in self.input_files_dir:
-            _, _, file = get_path_file_extension(modality)
+            _, _, file = utils.get_path_file_extension(modality)
             for b in [True, False]:
                 if b:
-                    image = StructureSegmentation.cut_area_from_image(modality, image_tumor_mask, True)
+                    image = utils.cut_area_from_image(modality, image_tumor_mask, True)
                     nib.save(image, self.out_dir + file + "-woTumor.nii.gz")
                 else:
-                    image = StructureSegmentation.cut_area_from_image(modality, image_tumor_mask, False)
+                    image = utils.cut_area_from_image(modality, image_tumor_mask, False)
                     nib.save(image, self.out_dir + file + "-withTumor.nii.gz")
 
     def segment_brain_part(self) -> None:
@@ -239,41 +123,44 @@ class StructureSegmentation:
         """
         brain_files = []
         for modality in self.input_files_dir:
-            _, _, file = get_path_file_extension(modality)
+            _, _, file = utils.get_path_file_extension(modality)
             brain_files.append(self.out_dir + file + "-woTumor.nii.gz")
 
-        StructureSegmentation.single_segmentation(self.out_dir + "wms_Brain", brain_files,
+        utils.single_segmentation(self.out_dir + "wms_Brain", brain_files,
                                                   len(self.brain_handling_classes))
         for i, seg_class in enumerate(self.brain_handling_classes):
             os.rename(self.out_dir + "wms_Brain_pve_" + str(i) + ".nii.gz", self.out_dir + seg_class + ".nii.gz")
 
-    def tumor_agnostic_mode(self) -> None:
+    def tumor_agnostic(self) -> None:
         """
         Runs tumor agnostic segmentation mode. The files are simply segmented like no tumor is present.
 
         :return: None
         """
-        self.single_segmentation(self.out_dir, self.input_files_dir, len(self.brain_handling_classes))
+        utils.single_segmentation(self.out_dir, self.input_files_dir, len(self.brain_handling_classes))
         for i, seg_class in enumerate(self.brain_handling_classes):
             os.rename(self.out_dir + "_pve_" + str(i) + ".nii.gz", self.out_dir + seg_class + ".nii.gz")
 
-    def bias_corrected_approach(self) -> None:
+    def bias_corrected(self) -> None:
         """
         Sub-routine for the bias corrected approach, where fast is run again on the cut area of the tumor segmentation.
 
         :return: None
         """
+        self.split_tumor_from_brain()
+        self.segment_brain_part()
+
         tumor_files = []
         # just returns classification based on intensity
         for modality in self.input_files_dir:
-            _, _, file = get_path_file_extension(modality)
+            _, _, file = utils.get_path_file_extension(modality)
             tumor_files.append(self.out_dir + os.sep + file + "-withTumor.nii.gz")
-        StructureSegmentation.single_segmentation(self.out_dir + os.sep + "tumor_class",
-                                                  tumor_files, len(list(self.tumor_class_mapping.keys())))
+        utils.single_segmentation(self.out_dir + os.sep + "tumor_class", tumor_files,
+                                  len(list(self.tumor_class_mapping.keys())))
         for i, seg_class in enumerate(list(self.tumor_class_mapping.keys())):
             os.rename(self.out_dir + "tumor_class_pve_" + str(i) + ".nii.gz", self.out_dir + seg_class + ".nii.gz")
 
-    def tumor_entity_weighted_approach(self) -> None:
+    def tumor_entity_weighted(self) -> None:
         """
         Sub-routine for the tumor entity weighted approach, where the actual tumor segmentation is used to create masks
         for cuts of the original image. The original intensities are normalised, which gives three entities with
@@ -281,15 +168,17 @@ class StructureSegmentation:
 
         :return: None
         """
+        self.split_tumor_from_brain()
+        self.segment_brain_part()
         tumor_masks = {}
         for key, value in self.tumor_class_mapping.items():
-            tumor_mask = StructureSegmentation.image2mask(self.tumor_seg_dir, value)
+            tumor_mask = utils.image2mask(self.tumor_seg_dir, value)
             tumor_masks[key] = nib.Nifti1Image(tumor_mask, self.affine)
         # get segmentation and separate in three classes, cut class-wise from mri and normalise
         for modality in self.input_files_dir:
-            _, _, file = get_path_file_extension(modality)
+            _, _, file = utils.get_path_file_extension(modality)
             for key in tumor_masks:
-                image = StructureSegmentation.cut_area_from_image(modality, tumor_masks[key], False)
+                image = utils.cut_area_from_image(modality, tumor_masks[key], False)
                 array = image.get_fdata()
                 array[array == 0] = -1
                 array = (array - array[array > 0].min()) / (array.max() - array[array > 0].min())
@@ -305,28 +194,22 @@ class StructureSegmentation:
 
         :return: None
         """
-        if self.mode not in self.modes:
+        if self.mode not in MODES:
             raise ValueError(f"Error: Selected mode: {self.mode} is not implemented. Function 'list_modes()' prints the"
                              f" possible options.")
-        if (self.mode is self.modes[1] or self.mode is self.modes[2]) and self.tumor_seg_dir is None:
+        if (self.mode is MODES[1] or self.mode is MODES[2]) and self.tumor_seg_dir is None:
             raise ValueError(f"Error: For selected mode '{self.mode}' the tumor_seg_dir should not be None.")
 
-        mkdir_if_not_exist(self.out_dir)
+        utils.mkdir_if_not_exist(self.out_dir)
 
         if self.mode == "tumor_agnostic":
-            self.tumor_agnostic_mode()
+            self.tumor_agnostic()
 
-        if self.mode in ["bias_corrected", "tumor_entity_weighted"]:
-            self.split_tumor_from_brain()
+        if self.mode == "bias_corrected":
+            self.bias_corrected()
 
-            self.segment_brain_part()
-
-            if self.mode == "bias_corrected":
-                self.bias_corrected_approach()
-
-            if self.mode == "tumor_entity_weighted":
-                self.tumor_entity_weighted_approach()
-
+        if self.mode == "tumor_entity_weighted":
+            self.tumor_entity_weighted()
 
         if self.remove_interim_files:
             files_to_remove = glob.glob(os.path.join(self.out_dir, '*Tumor.nii.gz'))
---
+++ oncostr/utils.py
@@ -0,0 +1,115 @@
+
+import fsl.wrappers.fslmaths
+import fsl.wrappers.fast
+import numpy as np
+import copy
+from pathlib import Path
+from typing import Union, Any
+import nibabel as nib
+import os
+
+
+def mkdir_if_not_exist(directory: str) -> str:
+    """
+    Makes directory if not exists and returns the string.
+
+    :param directory: String
+    """
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    return directory
+
+
+def split_path(s: str) -> tuple[str, str]:
+    """
+    Splits Filepath into file and path
+
+    *Arguments:*
+        s: String
+
+    *Example:*
+        file, path = splitPath(s)
+    """
+    import os
+    f = os.path.basename(s)
+    p = s[:-(len(f)) - 1]
+    return str(f), str(p)
+
+
+def get_path_file_extension(input_file: str) -> tuple[str, str, str]:
+    """
+    Returns path, the filename and the filename without extension.
+
+    *Arguments:*
+        input_file: String
+
+    *Example:*
+        path, file, file_wo_extension = get_path_file_extension(input_file)
+    """
+    file, path = split_path(input_file)
+    file_wo_extension = Path(Path(input_file).stem).stem
+    return path, file, file_wo_extension
+
+
+def cut_area_from_image(input_image: str, area_mask: nib.Nifti1Image,
+                            inverse: bool = False) -> Union[None, nib.Nifti1Image]:
+    """
+    Cuts an area of that image.
+
+    :param input_image: String of path to Nifti image
+    :param area_mask: Mask array
+    :param inverse: Bool, true for inverse cut
+
+    :return: optionally returns the image or writes it next to the input image
+    """
+    if inverse:
+        area_mask = fsl.wrappers.fslmaths(area_mask).mul(-1).add(1).run()
+
+    return fsl.wrappers.fslmaths(input_image).mul(area_mask).run()
+
+
+def image2array(image_dir: str) -> tuple[Any, Any, Any]:
+    """
+    Takes a directory of an image and gives a numpy array.
+
+    :param image_dir: String of a Nifti image directory
+    :return: numpy array of image data, shape, affine
+    """
+    orig_image = nib.load(image_dir)
+    return copy.deepcopy(orig_image.get_fdata()), orig_image.shape, orig_image.affine
+
+
+def image2mask(image_dir: str, compartment: int, inner_compartments: list[int] = None) -> np.ndarray:
+    """
+    Gives deep copy of original image with selected compartments.
+
+    :param image_dir: String to Nifti image
+    :param compartment: Int, identifier of compartment that shall be filtered
+    :param inner_compartments: List of inner compartments that also are included in the mask
+
+    :return mask: Numpy array of the binary mask
+    """
+    mask, _, _ = image2array(image_dir)
+    unique = list(np.unique(mask))
+    unique.remove(compartment)
+    for outer in unique:
+        mask[np.isclose(mask, outer)] = 0.0
+    mask[np.isclose(mask, compartment)] = 1.0
+    if inner_compartments is not None:
+        for comp in inner_compartments:
+            mask[np.isclose(mask, comp)] = 1.0
+            unique.remove(comp)
+    return mask
+
+
+def single_segmentation(basename: str, files_list: list[str], n_classes: int) -> None:
+    """
+    runs fast segmentation algorithm in default with variable input files.
+
+    :param basename: String for base name of outputfiles
+    :param files_list: List of input images
+    :param n_classes: Number of segmentation classes
+
+    :return: None
+    """
+    fsl.wrappers.fast(files_list, basename, n_classes)
